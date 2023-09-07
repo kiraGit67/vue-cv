8123f7e742ff5538f4f668c36c104330ff06eed1
kiraGit67 <kirsten@sais.de>
2023-09-07 22:04:08 +0200

    adds state for dependentJobs

--- script.js
+++ script.js
@@ -20,6 +20,55 @@ const app = Vue.createApp({
         nationality: "deutsch",
         photoSrc: "img/kirsten_mail.jpg",
       },
+      dependentJobs: [
+        {
+          id: 1,
+          duration: "05/2018 - 11/2018",
+          position: "Medien-Beraterin",
+          company: "Stadtspiegel Dortmund",
+          address: "Westerfilder Straße 22, 44239 Dortmund",
+          tasks: [
+            "Telefon-Akquise von Zeitungsanzeigen",
+            "Versenden von Angeboten",
+          ],
+        },
+        {
+          id: 2,
+          duration: "04/2018",
+          position: "Büro-Aushilfskraft",
+          company: "H.W. Nowak",
+          address: "Aplerbecker Straße 432, 44287 Dortmund",
+          tasks: [
+            "Rechnungen und Angebote schreiben",
+            "Telefonische Kundenberatung",
+          ],
+        },
+        {
+          id: 3,
+          duration: "12/2004 - 11/2007",
+          position: "Hotline-Mitarbeiterin / Bürofachkraft",
+          company: "Business-Partner-System GmbH",
+          address: "Duisburger Straße 375, 46049 Oberhausen",
+          tasks: [
+            "Kundenbetreuung an der Hotline",
+            "Auswertung des Schriftverkehrs",
+            "Entwurf von Marketing-Materialien",
+          ],
+        },
+        {
+          id: 4,
+          duration: "06/1987 - 07/1994",
+          position: "Druckvorlagenherstellerin",
+          company: "Hilpert GmbH & Co. KG",
+          address: "Girardetstraße 58-64, 45131 Essen",
+          tasks: [
+            "Bild-Text-Composing",
+            "Bildbearbeitung",
+            "Farbkorrekturen",
+            "Originalmontage bis zum Endfilm",
+          ],
+        },
+      ],
     };
   },
 }).mount("#app");
